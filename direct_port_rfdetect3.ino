449f275f3e7cab537909958e54c8feec08455423
mkylman <66576922+mkylman@users.noreply.github.com>
2020-08-14 21:36:48 -0400

    Update direct_port_rfdetect3.ino

--- direct_port_rfdetect3.ino
+++ direct_port_rfdetect3.ino
@@ -7,43 +7,59 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// SQUASH can use range of 0 - 7
-#define SQUASH 3
+// 0b01000000 - enable ADSC to start a conversion
+#define READADC ADCSRA |= (1 << ADSC)
 
-byte aRead() {
+// wait until ADSC is reset and read is done
+#define WAITADC while (ADCSRA & (1 << ADSC))
+
+// combine ADCL and ADCH for full resolution
+#define TENBIT (ADCL | ADCH<<8)
+
+// REFS1 - 0, REFS0 - 1 = VCC as reference
+// MUX3:0 - 0 = use ADC0
+#define CONFIGADC ADMUX |= 0b01000000
+
+// enable ADC, set prescalers to max
+#define ENABLEADC ADCSRA |= 0b10000111
+
+// 0b00100000 - LED ON
+#define LEDON  PORTB |= (1 << PORTB5)
+// 0b00000000 - LED OFF
+#define LEDOFF PORTB &= (0 << PORTB5)
+
+int16_t aRead() {
   _delay_ms(1);
-  ADCSRA |= (1 << ADSC); // 0b01000000 - enable ADSC to start a conversion
-  while (ADCSRA & (1 << ADSC)); // wait until ADSC is reset and read is done
-  
-  return ((ADCL | ADCH<<8) >> SQUASH); // smash it way down
+  READADC;
+  WAITADC;
+  return (TENBIT);
 }
 
-void minMax(byte val, byte *oMin, byte *oMax){
+void minMax(int16_t val, int16_t *oMin, int16_t *oMax){
   if (val < *oMin) *oMin = val;
   if (val > *oMax) *oMax = val;
 }
 
 int main(void) {
-  byte baseMax = 0;
-  byte baseMin = 255;
-  
-  DDRB  |= (1 << PORTB5);// 0b00100000 - set led pin to output
-  ADMUX |= 0b01000000;   // REFS1 to 0, REFS0 to 1: set VCC as reference voltage
-                         // select ADC0
-  ADCSRA |= 0b10000111;  // enable ADC, set prescalers to max
+  DDRB  |= (1 << PORTB5);// set led pin to output
+  CONFIGADC;
+  ENABLEADC;
+
+  int16_t baseMax = 0;
+  int16_t baseMin = 1023;
   
   for (byte i = 0; i < 30; i++)
     minMax(aRead(), &baseMin, &baseMax);
   
   while (1) {
-    byte newMax = baseMin;
-    byte newMin = baseMax;
+    int16_t newMax = baseMin;
+    int16_t newMin = baseMax;
+    
     for (byte i = 0; i < 20; i++)
       minMax(aRead(), &newMin, &newMax);
 
     if (newMin > baseMin && newMax < baseMax)
-      PORTB |= (1<<PORTB5); // 0b00100000 - LED ON
-    else
-      PORTB &= (0<<PORTB5); // 0b00000000 - LED OFF
+      LEDON;
+    else LEDOFF;
   }
 }
